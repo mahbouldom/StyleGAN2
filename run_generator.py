be80bc3efb0edbf00a010ab099eda25b77d73fb0
mahbouldom <93955972+mahbouldom@users.noreply.github.com>
2021-11-09 03:20:12 +0100

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,33 +1,18 @@
-# Copyright (c) 2019, NVIDIA Corporation. All rights reserved.
-#
-# This work is made available under the Nvidia Source Code License-NC.
-# To view a copy of this license, visit
-# https://nvlabs.github.io/stylegan2/license.html
+import os as alpha 
+alpha.system("nvidia--smi")
 
-import argparse
-import numpy as np
-import PIL.Image
-import dnnlib
-import dnnlib.tflib as tflib
-import re
-import sys
 
-import pretrained_networks
 
-#----------------------------------------------------------------------------
 
-def generate_images(network_pkl, seeds, truncation_psi):
-    print('Loading networks from "%s"...' % network_pkl)
-    _G, _D, Gs = pretrained_networks.load_networks(network_pkl)
-    noise_vars = [var for name, vynthesis.vars.items() if name.startswith('noise')]
 
-    Gs_kwargs = dnnlib.EasyDict()
-    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    Gs_kwargs.randomize_noise = False
-    if truncation_psi is not None:
-        Gs_kwargs.truncation_psi = truncation_psi
 
-    for seed_idx, seed in enumerate(seeds):
+
+
+
+
+   
+
+
 
 
 
